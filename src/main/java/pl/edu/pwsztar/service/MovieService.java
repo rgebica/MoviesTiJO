bb69ffe543bd2f6b4cb47fc2b842851349bb47f6
rgebica <rgebica@gmail.com>
2020-03-19 22:39:21 +0100

    Showing films with year of production

---
+++ src/main/java/pl/edu/pwsztar/service/MovieService.java
@@ -0,0 +1,11 @@
+package pl.edu.pwsztar.service;
+
+import pl.edu.pwsztar.domain.dto.CreateMovieDto;
+import pl.edu.pwsztar.domain.dto.MovieDto;
+
+import java.util.List;
+
+public interface MovieService {
+
+    List<MovieDto> findAll();
+}
